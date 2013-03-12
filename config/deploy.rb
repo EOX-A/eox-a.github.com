77de70974463f0fa4cffcdf7f938f06a456da0e7
Marko Locher <marko.locher@eox.at>
2013-03-12 16:56:39 +0100

    Fixed whitespace issue in config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,14 +17,14 @@ ssh_options[:forward_agent] = true
 
 before 'deploy:update', 'deploy:update_jekyll'
 namespace :deploy do
-  [:start, :stop, :restart, :finalize_update].each do |t|
-    desc "#{t} task is a no-op with jekyll"
-    task t, :roles => :app do ; end
-  end
-  
-  desc 'Run jekyll to update site before uploading'
-    task :update_jekyll do
-      %(rm -rf _site/*)
-      %(&& jekyll)
-    end
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
+  end
+
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    %(rm -rf _site/*)
+    %(&& jekyll)
+  end
 end
